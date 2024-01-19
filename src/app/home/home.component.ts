2ff845cd75ca3ea4ee1a3be79140be04eaf8f86b
rafa9694 <rafa9694@gmail.com>
2024-01-19 08:03:49 -0300

    Ajustando tela de home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGear } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, FontAwesomeModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  configurarIcon = faGear;
 }
